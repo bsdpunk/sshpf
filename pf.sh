6843772ecb6b1d76d8e8fdc085b3826ed5c42fb0
dusty <bsdpunk@gmail.com>
2016-09-01 12:01:12 -0500

    sshpf

---
+++ pf.sh
@@ -0,0 +1,18 @@
+#!/bIN/BASh
+
+#get token
+printf "Enter The Remote Port: "
+read -r PORT 
+printf "Enter The IP of the routable machine: "
+read -r REMOTE
+printf "Enter The IP of the Machine you are targeting, with the port forward: "
+read -r TARGET
+
+
+#   if [ -z $CMD ]
+#    then
+#        CMD=""
+#    else
+#        CMD="?access_token=$CMD"
+#    fi
+#
